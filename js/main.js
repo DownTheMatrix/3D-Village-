b52118bcbddd970cc7153b92933db7dbbe56b740
DownTheMatrix <suija87@gmail.com>
2019-10-04 16:57:24 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/* Toggle Modal: On/Off */
+const modal = document.querySelector(".modal");
+
+const fadeModal = setInterval(function() {
+    modal.style.opacity = 1;
+}, 2000);
+
+function rotateCamera() {
+    window.addEventListener("keydown", function() {
+        if(event.code === "Enter") {
+            modal.style.opacity = 0;
+            clearInterval(fadeModal);
+            controls.autoRotate = true;
+        } else if(event.code === "Space") {
+            modal.style.opacity = 1;
+            controls.autoRotate = false;
+        }
+    })
+}
+rotateCamera();
+
+/* Setup Scene */
+var scene = new THREE.Scene();
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+/* Setup Camera */
+var camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
+
+/* Setup Controls */
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 5, 20);
+// rotate camera automatically: controls.autoRotate = true;
+
+/* Import and Load Model */
+var loader = new THREE.GLTFLoader();
+loader.load("../models/medieval-village/scene.gltf", function(gltf) {
+    scene.add(gltf.scene);
+}, undefined, function(error) {
+    console.error(error);
+    }
+);
+
+/* Render Scene */
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+/* Initialize Once DOM is ready */
+document.addEventListener("DOMContentLoaded", animate);
